ebbe7168b6b77bcdc857f9735de806c50bac0f2e
vagrant <vagrant@homestead>
2017-11-03 17:35:00 +0000

    log4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="{{ url('css/docs.min.css') }}">
         <!-- Styles -->
         <style>
             html, body {
