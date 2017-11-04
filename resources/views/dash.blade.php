8506ed8ffe3bf5b57d7ff079c6be142f439799f1
vagrant <vagrant@homestead>
2017-11-04 09:56:14 +0000

    log6

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -11,6 +11,14 @@
     <nav class="navbar navbar-light bg-light">
       <span class="navbar-brand mb-0 h1">Larabuk</span>
     </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-3">Hello, World!</h1>
+        <p class="lead">Selamat datand di Larabuk, Belajar Laravel gaya Batuk</p>
+        <p class="lead">Ceritanya ini perpustakaan. Silahkan daftar dan mulai pinjam buku biar ndak panik</p>
+      </div>
+    </div>
     <script type="text/javascript" src="{{ url('bootstrap.min.js') }}"></script>
   </body>
 </html>
