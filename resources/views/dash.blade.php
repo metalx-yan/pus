6a7cc8531dd46681101bef13dd18159e8ba368a3
vagrant <vagrant@homestead>
2017-11-04 10:11:22 +0000

    log7

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -9,7 +9,11 @@
   </head>
   <body>
     <nav class="navbar navbar-light bg-light">
-      <a href="{{ route('root') }}"><span class="navbar-brand mb-0 h1">Larabuk</span></a>
+        <a href="{{ route('root') }}"><span class="navbar-brand mb-0 h1">Larabuk</span></a>
+        <div class="form-inline">
+          <span class="navbar-brand"><h6>Login</h6></span>
+          <span class="navbar-brand"><h6>Daftar</h6></span>
+        </div>
     </nav>
 
     <div class="jumbotron jumbotron-fluid">
